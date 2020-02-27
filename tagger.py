18a5ad00955e0e99b41e47b7f9098b49fb4e797a
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-02-27 11:38:03 -0500

    creates proper list now
    until now had wrong assumptions. now list is created for sets of [tag-1, word, tag]. now need create dictionaries for word frequency, POS frequency, (word,tag) frequency, and (tag-1,tag) frequency.

--- tagger.py
+++ tagger.py
@@ -10,12 +10,26 @@ def format(text):
 
 #creates: [[word, POS],
 #          [word, POS],...]
+#
+#now need: [[prevPOS, word, POS],
+#          [prevPOS, word, POS],...]
+#
+#rem after .,!,? prevPOS is <START> not the actual tag
 def toWordList(text):
     text = re.sub(r'\\\/', r'\\', text)#to allow splitting at /
     pairs = text.split()
     dpairs = []
+    start = '<START>'
+    POSm1 = start
     for pair in pairs:
-        dpairs.append(pair.split('/'))
+        wordPOS = pair.split('/')
+        wordPOS.insert(0, POSm1)
+        POSwordPOS = wordPOS
+        dpairs.append(POSwordPOS)
+        if POSwordPOS[2] == '.' or POSwordPOS[2] == '?' or POSwordPOS[2] == '!':
+            POSm1 = start
+        else:
+            POSm1 = POSwordPOS[2]
     #to put back \/ for consistency with prof expected op:
     for pair in dpairs:
         for itm in pair:
@@ -68,14 +82,20 @@ with open(trainFile, 'r+') as f:
     trainText = ' '.join(lines)
 
 
-guide = createGuide(createFreqency(toWordList(format(trainText))))
+sets = toWordList(format(trainText))
 
+
+#dont delete until you have whole correction done so you can see proper order
+#guide = createGuide(createFreqency(toWordList(format(trainText))))
+
+####################################FUCK, NEED TO MAKE WHOLE THING BIGRAM MODEL. FUCK
 
 #TESTING
 #print(trainText)
 #print(type(trainText))
 #print(toWordList(trainText))
-print(guide)
+#print(guide)
+print(sets)
 
 
 with open(testFile, 'r+') as f:
